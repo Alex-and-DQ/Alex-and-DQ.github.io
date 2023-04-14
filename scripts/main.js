4e194596610ffa4ac8d39bf1951979d974f9f4a1
Alex <211250006.smail.nju.edu.cn>
2023-04-14 21:01:24 +0800

    title:更新了标题

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+        myHeading.textContent = '酷毙了，' + myName;
     }
 }
 if(!localStorage.getItem('name')) {
